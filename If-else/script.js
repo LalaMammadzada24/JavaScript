74f2023d51f3a07abead532daaac6283ac0197d8
Lala Mammadzada <160578995+LalaMammadzada24@users.noreply.github.com>
2024-07-13 13:59:11 +0400

    Add files via upload

---
+++ If-else/script.js
@@ -0,0 +1,122 @@
+// const time = 15;
+
+// if (time > 5 && time < 13) {
+//     console.log('good morning');
+// }
+// else if (time >= 13 && time <= 18) {
+//     console.log('good afternoon');
+// }
+// else if (time > 19 && time <= 21) {
+//     console.log('good evening');
+// }
+// else if (time > 21 && time <= 23) {
+//     console.log('good night');
+// }
+// else {
+//     console.log('wrong time ');
+// }
+
+
+
+//==============================================================================================
+
+
+
+// const age = 19;
+
+// if (age > 0 && age <= 13) {
+//     console.log('usaq');
+// }
+// else if (age >= 14 && age <= 18) {
+//     console.log('yeniyetme ');
+// }
+// else if (age > 18 && age <= 35) {
+//     console.log('genc ');
+// }
+// else if (age > 35 && age <= 50) {
+//     console.log('orta ');
+// }
+// else if (age > 50 && age < 70) {
+//     console.log('yasli ');
+// }
+// else if (age > 70) {
+//     console.log('R.I.P ');
+// }
+
+
+//=================================================================
+
+
+
+// let info = 5;
+
+// switch (info) {
+//     case 5:
+//         info = 100
+//         console.log("this is 5");
+//         if (info >= 20) {
+//             console.log("this is 100");
+//         }
+//         break;
+//     case 10:
+//         console.log("this is 10");
+//         break;
+//     default:
+//         console.log("this is default");
+//         break;
+// }
+
+
+//===================================================================
+
+
+// const hour=10;
+// money=hour*3;
+// console.log("Earned money:" + money + "$");
+
+// const productOne=['bread','water','oil,'];
+// const productTwo=['onion','milk','egg,'];
+// const productThree=['meat','cola','fruits','fish,'];
+// const productFour=productOne+productTwo;
+// const productFive=productTwo+productThree;
+
+
+// if (money >= 10 && money < 20) {
+//     console.log( 'You can buy them:' +productOne);
+// }
+// else if (money >= 20 && money < 40) {
+//     console.log('You can buy them:' +productTwo);
+// }
+// else if (money >=40 && money <70) {
+//     console.log('You can buy them:' +productThree);
+// }
+// else if (money >=70 && money < 120) {
+//     console.log('You can buy them:' +productFour);
+// }
+// else if (money >= 120) {
+//     console.log('You can buy them:' +productFive);
+// }
+// else   {
+//     console.log('You can\'t buy anything');
+// }
+
+
+let a=10;
+
+while (a>=1) {
+    console.log(a);
+    a--;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
